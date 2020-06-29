878331d9a506ee4f9b5616d93df65bf37596a8cc
MasatakaIshii <masaradio78@gmail.com>
2020-06-29 22:15:34 +0200

    Add few instructions in gitignore

---
+++ core/utils/Singleton.py
@@ -0,0 +1,14 @@
+class Singleton:
+    _instance = None
+    @staticmethod
+    def getInstance():
+        """ Static access method """
+        if Singleton._instance == Nonu:
+            Singleton()
+        return Singleton._instance
+    def __init__(self):
+        """ Virtyally private constructor """
+        if Singleton._instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton._instance = self
