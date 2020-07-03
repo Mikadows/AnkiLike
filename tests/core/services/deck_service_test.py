7219d1b6e1c86ff5723bae53c85fc751daba6545
MasatakaIshii <masaradio78@gmail.com>
2020-07-03 13:44:54 +0200

    Add tests of DeckService

--- tests/core/services/deck_service_test.py
+++ tests/core/services/deck_service_test.py
@@ -17,14 +17,25 @@ class DeckServiceTest(unittest.TestCase):
         self.deck_service = DeckService(self.data)
 
     def test_get_deck_should_return_deck_present_in_data_box(self):
-        self.assertIsInstance(self.deck_service.get_deck(0), Deck,
+        current_decks = self.data.box.decks
+        current_decks.append(Deck("2nd deck", [self.card]))
+        self.data.box.decks = current_decks
+
+        self.assertIsInstance(self.deck_service.find_deck_by_index(0), Deck,
                               "incorrect deck instance")
-        self.assertEqual(self.deck_service.get_deck(0), self.deck,
+        self.assertEqual(self.deck_service.find_deck_by_index(0), self.deck,
+                         "incorrect deck value")
+        self.assertEqual(self.deck_service.find_deck_by_index(1).name, "2nd deck",
                          "incorrect deck value")
 
     def test_get_decks_should_not_raise_exception_when_index_out_bound_number_decks(self):
         number_deck = len(self.data.box.decks)
         try:
-            self.deck_service.get_deck(number_deck)
+            self.deck_service.find_deck_by_index(number_deck)
         except IndexError:
-            self.fail(format("deck_service.get_deck({}) raised IndexError unexpectedly!", number_deck))
+            self.fail("deck_service.get_deck({0}) raised IndexError unexpectedly!".format(number_deck))
+
+    def test_get_decks_should_return_None_when_index_out_bound_number_decks(self):
+        number_deck = len(self.data.box.decks)
+        self.assertIsNone(self.deck_service.find_deck_by_index(number_deck),
+                          "deck value is not None")
