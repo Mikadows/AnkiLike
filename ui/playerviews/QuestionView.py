f37ceccec0e58043976c45855cba6aaa2470c631
MasatakaIshii <masaradio78@gmail.com>
2020-07-03 18:12:35 +0200

    Reput QuestionView like before and add back button

--- ui/playerviews/QuestionView.py
+++ ui/playerviews/QuestionView.py
@@ -22,32 +22,30 @@ class QuestionView(tkinter.Frame):
         self.font_title = tkFont.Font(family="Lucida Grande", size=20)
         self.line = tkinter.Label(text=self.str_line, font=self.font_title)
         self.title = tkinter.Label(text="AnkiLike - Player Mode", font=self.font_title)
-        self.title.pack()
-        self.line.pack()
 
-        self.frame_menu = tkinter.LabelFrame(self.app, text="Question")
-        self.frame_menu.pack(pady=20)
-
-        self.back_btn = tkinter.Button(self.frame_menu, text="Back", command=self.load_back_view)
-
-        self.deck_name = tkinter.Label(self.frame_menu, text="Deck name : " + deck.name, font=("Lucida Grande", 15))
-
+        self.deck_name = tkinter.Label(text="Deck name : " + deck.name, font=self.font_title)
+        self.deck_name.config(font=("Lucida Grande", 15))
+        self.question_title = tkinter.Label(text="Question :", font=("Lucida Grande", 15))
         self.current_card = self.question_controller.draw_card()
-        self.current_content = tkinter.Label(self.frame_menu, text=self.current_card.question, font=("Lucida Grande", 15))
-        self.button_show_answer = tkinter.Button(self.frame_menu, text="Show Answer", command=self._call_show_answer)
-
+        self.current_content = tkinter.Label(text=self.current_card.question, font=("Lucida Grande", 20))
+        self.button_show_answer = tkinter.Button(text="Show answer", command=self._call_show_answer)
+        self.back_button = tkinter.Button(text="Return list decks", command=self._back_player_view)
         self._create_player_deck_view()
 
     def _create_player_deck_view(self):
+        self.title.pack()
+        self.line.pack()
         self.deck_name.pack()
-        # self.current_content.pack()
+        self.question_title.pack()
+        self.current_content.pack()
+        self.button_show_answer.pack()
+        self.back_button.pack()
+
+    def _back_player_view(self):
+        PlayerView(self.app)
 
     def _call_show_answer(self):
-        # self.current_content.config()
-        print("answer")
-
-    def load_back_view(self):
-        PlayerView(self.app)
+        self.current_content.config()
 
     def clear(self):
         for widget in self.app.winfo_children():
