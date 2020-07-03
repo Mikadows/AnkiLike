a8adaf1f1a83e9661c07f4347e34c4b58550bafc
MasatakaIshii <masaradio78@gmail.com>
2020-07-03 22:51:01 +0200

    In question controller method update card validation level, add card service method update validation level

--- ui/playerviews/QuestionController.py
+++ ui/playerviews/QuestionController.py
@@ -19,5 +19,6 @@ class QuestionController:
         return self.card_service.data.box.decks[self.deck_index].cards[self.current_card_index]
 
     def update_card_validation_level(self, is_validate: bool, master=None):
+        self.card_service.update_validation_level(self.deck_index, self.current_card_index, is_validate)
         from ui.playerviews.QuestionView import QuestionView
         QuestionView(self.deck_index, master)
