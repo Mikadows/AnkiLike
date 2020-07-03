e7298578bb2378bac2666438733adbbbb06ee396
MasatakaIshii <masaradio78@gmail.com>
2020-07-03 13:47:43 +0200

    In PlayerController, add deck service attribute

--- ui/playerviews/PlayerController.py
+++ ui/playerviews/PlayerController.py
@@ -1,11 +1,12 @@
 from core.Data import Data
+from core.services import DeckService
 from ui.playerviews.QuestionView import QuestionView
 
 
 class PlayerController:
-    def __init__(self, data: Data):
+    def __init__(self, data: Data, deck_service: DeckService):
         self.data = data
+        self.deck_service = deck_service
 
     def play(self, master=None, index: int = -1):
-        decks = self.data.box.decks
-        QuestionView(decks[index], master)
+        QuestionView(self.deck_service.find_deck_by_index(index), master)
