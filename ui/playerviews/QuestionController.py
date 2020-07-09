56d65ab7368a3da305eb641cf3e5095f25a70eb8
MasatakaIshii <masaradio78@gmail.com>
2020-07-09 16:31:39 +0200

    fix call of get card randomly and in init set property of card_service

--- ui/playerviews/QuestionController.py
+++ ui/playerviews/QuestionController.py
@@ -1,24 +1,32 @@
+from core.Data import Data
+from core.classes.Card import Card
 from core.services import CardService
+from core.utils.ValuesRatioLister import ValuesRatioLister
 
 
 class QuestionController:
-    def __init__(self, deck_index: int, card_service: CardService):
+    def __init__(self, deck_index: int, card_service: CardService, last_card: Card):
         self.deck_index = deck_index
         self.card_service = card_service
+        self.card_service.data = Data()
+        self.card_service.values_ratio_lister = ValuesRatioLister(2/3)
+        self.card_service.deck_index = deck_index
+        self.card_service.current_card = last_card
 
-    def _get_current_card_index(self):
-        return self._current_card_index
+    def __get_current_card(self):
+        return self._current_card
 
-    def _set_current_card_index(self, current_card_index):
-        self._current_card_index = current_card_index
+    def __set_current_card(self, current_card):
+        self._current_card = current_card
 
-    current_card_index = property(_get_current_card_index, _set_current_card_index)
+    current_card = property(__get_current_card, __set_current_card)
 
     def draw_card(self):
-        self.current_card_index = self.card_service.get_card_index_randomly()
-        return self.card_service.data.box.decks[self.deck_index].cards[self.current_card_index]
+        self.current_card = self.card_service.get_card_randomly()
+        return self.current_card
 
     def update_card_validation_level(self, is_validate: bool, master=None):
-        self.card_service.update_validation_level(self.deck_index, self.current_card_index, is_validate)
+        current_card_index = self.card_service.data.box.decks[self.deck_index].cards.index(self.current_card)
+        self.card_service.update_validation_level(self.deck_index, current_card_index, is_validate)
         from ui.playerviews.QuestionView import QuestionView
-        QuestionView(self.deck_index, master)
+        QuestionView(self.deck_index, master, last_card=self.card_service.current_card)
