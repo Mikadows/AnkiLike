289ca192c1421c662e6632f498e11085283cedff
MasatakaIshii <masaradio78@gmail.com>
2020-07-09 10:44:36 +0200

    Change CardSelector to RandomLister

---
+++ core/utils/RandomLister.py
@@ -0,0 +1,54 @@
+from core.classes.Deck import Deck
+
+
+class RandomLister:
+
+    def __init__(self, values, ratio: float, list_len=100):
+        self.__values = values
+        self.__ratio = ratio
+        self.__list_len = list_len
+
+    def __get_values(self):
+        return self.__values
+
+    def __set_values(self, deck: Deck):
+        self.__values = deck
+
+    values = property(__get_values, __set_values)
+
+    def __get_ratio(self):
+        return self.__ratio
+
+    def __set_ratio(self, ratio):
+        self.__ratio = ratio
+
+    ratio = property(__get_ratio, __set_ratio)
+
+    def get_random_list(self):
+        """
+        this function return integer list that contains that
+        allocates values according to the ratio
+        """
+        result = []
+        available_cases = self.__list_len
+        # validation_levels = self.__get_validation_levels_present_in_cards()
+        for i in range(len(self.__values)):
+            cases_to_take = round(available_cases * self.__ratio)
+            if available_cases - cases_to_take <= 1:
+                if cases_to_take > 1:
+                    cases_to_take -= 1
+
+            result += [self.__values[i]] * cases_to_take
+            available_cases -= cases_to_take
+            if i == len(self.__values) - 1:
+                result += [self.__values[i]] * available_cases
+
+        return result
+
+    # def __get_validation_levels_present_in_cards(self):
+    #     result = []
+    #     vl_list = [card.validation_level for card in self.__values.cards]
+    #
+    #     [result.append(vl) for vl in vl_list if vl not in result]
+    #
+    #     return result
--- tests/core/utils/random_lister_test.py
+++ tests/core/utils/random_lister_test.py
@@ -1,62 +1,55 @@
 import unittest
 
-from core.classes.Card import Card
-from core.classes.Deck import Deck
-from core.utils.CardSelector import CardSelector
+from core.utils.RandomLister import RandomLister
 
 
-class CardSelectorTest(unittest.TestCase):
+class RandomListerTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.__max_validation_level = 4
-        deck = self.create_deck_with_define_cards_validation_level([5])
-        self.__cardSelector = CardSelector(deck, 1 / 2)
+        self.__cardSelector = RandomLister([-1], 1 / 2)
 
     def test_get_list_validations_should_return_list_length_100(self):
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len(result), 100)
 
     def test_get_list_validations_should_return_list_same_number_when_cards_only_minus_one_validation_level(self):
-        deck = self.__cardSelector.deck
-        self.assertTrue(all(map(lambda card: card.validation_level == -1, deck.cards)))
-
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertTrue(all(map(lambda number: number == -1, result)))
 
     def test_get_list_validations_should_return_list_same_number_when_cards_only_have_same_validation_level(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([0, 4])
-        result = self.__cardSelector.get_list_validations()
+        self.__cardSelector.values = [0]
+        result = self.__cardSelector.get_random_list()
         self.assertTrue(all(map(lambda number: number == 0, result)))
 
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([0, 0, 3])
-        result = self.__cardSelector.get_list_validations()
+        self.__cardSelector.values = [1]
+        result = self.__cardSelector.get_random_list()
         self.assertTrue(all(map(lambda number: number == 1, result)))
 
     def test_get_list_validations_should_return_50_cards_when_ratio_half_and_2_different_validation_level(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([2, 4])
+        self.__cardSelector.values = [-1, 0]
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len([number for number in result if number == -1]), 50)
         self.assertEqual(len([number for number in result if number == 0]), 50)
 
     def test_get_list_validations_should_return_50_cards_when_ratio_half_and_2_not_successive_validation_level(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([2, 0, 4])
+        self.__cardSelector.values = [-1, 1]
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len([number for number in result if number == -1]), 50)
         self.assertEqual(len([number for number in result if number == 0]), 0)
         self.assertEqual(len([number for number in result if number == 1]), 50)
 
     def test_get_list_validations_should_return_two_thirds_lower_validation_level_and_manage_same_way_for_rest(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([3, 1, 0, 4])
+        self.__cardSelector.values = [-1, 0, 2]
         self.__cardSelector.ratio = 3 / 4
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len([number for number in result if number == -1]), 75)
         self.assertEqual(len([number for number in result if number == 0]), 19)
@@ -64,10 +57,10 @@ class CardSelectorTest(unittest.TestCase):
         self.assertEqual(len([number for number in result if number == 2]), 6)
 
     def test_get_list_validations_should_return_al_least_one_case(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([3, 1, 2, 7, 4, 4])
+        self.__cardSelector.values = [-1, 0, 1, 2, 3, 4]
         self.__cardSelector.ratio = 2 / 3
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len([number for number in result if number == -1]), 67)
         self.assertEqual(len([number for number in result if number == 0]), 22)
@@ -77,10 +70,10 @@ class CardSelectorTest(unittest.TestCase):
         self.assertEqual(len([number for number in result if number == 4]), 1)
 
     def test_get_list_validations_should_return_only_validation_level_max_when_cards_contain_not_below_level(self):
-        self.__cardSelector.deck = self.create_deck_with_define_cards_validation_level([0, 0, 0, 0, 0, 4])
+        self.__cardSelector.values = [4]
         self.__cardSelector.ratio = 5 / 6
 
-        result = self.__cardSelector.get_list_validations()
+        result = self.__cardSelector.get_random_list()
 
         self.assertEqual(len([number for number in result if number == -1]), 0)
         self.assertEqual(len([number for number in result if number == 0]), 0)
@@ -89,19 +82,19 @@ class CardSelectorTest(unittest.TestCase):
         self.assertEqual(len([number for number in result if number == 3]), 0)
         self.assertEqual(len([number for number in result if number == 4]), 100)
 
-    def create_deck_with_define_cards_validation_level(self, levels) -> Deck:
-        list_cards = []
-        validation_level = -1
-
-        for i in range(len(levels)):
-            if validation_level > self.__max_validation_level:
-                break
-            for y in range(levels[i]):
-                title = 'title_{}_{}'.format(y, validation_level)
-                question = 'question_{}_{}'.format(y, validation_level)
-                answer = 'answer_{}_{}'.format(y, validation_level)
-                card = Card(title, question, answer, validation_level)
-                list_cards.append(card)
-            validation_level += 1
-
-        return Deck('current_deck', list_cards)
+    # def create_deck_with_define_cards_validation_level(self, levels) -> Deck:
+    #     list_cards = []
+    #     validation_level = -1
+    #
+    #     for i in range(len(levels)):
+    #         if validation_level > self.__max_validation_level:
+    #             break
+    #         for y in range(levels[i]):
+    #             title = 'title_{}_{}'.format(y, validation_level)
+    #             question = 'question_{}_{}'.format(y, validation_level)
+    #             answer = 'answer_{}_{}'.format(y, validation_level)
+    #             card = Card(title, question, answer, validation_level)
+    #             list_cards.append(card)
+    #         validation_level += 1
+    #
+    #     return Deck('current_deck', list_cards)
