72b32a1cec2a3f387406984c44a6f6e0d8082327
MasatakaIshii <masaradio78@gmail.com>
2020-07-09 12:45:27 +0200

    Change values ratio lister method name

--- core/utils/ValuesRatioLister.py
+++ core/utils/ValuesRatioLister.py
@@ -3,18 +3,8 @@ from core.classes.Deck import Deck
 
 class ValuesRatioLister:
 
-    def __init__(self, values, ratio: float, list_len=100):
-        self.__values = values
+    def __init__(self, ratio: float):
         self.__ratio = ratio
-        self.__list_len = list_len
-
-    def __get_values(self):
-        return self.__values
-
-    def __set_values(self, deck: Deck):
-        self.__values = deck
-
-    values = property(__get_values, __set_values)
 
     def __get_ratio(self):
         return self.__ratio
@@ -24,7 +14,7 @@ class ValuesRatioLister:
 
     ratio = property(__get_ratio, __set_ratio)
 
-    def get_random_list(self, int_values, available_cases=100):
+    def get_values_ratio_list(self, int_values, available_cases=100):
         """
         this function return integer list that contains that
         allocates values according to the ratio
@@ -44,6 +34,6 @@ class ValuesRatioLister:
 
         if len(int_values) > 1:
             int_values.pop(0)
-            result += self.get_random_list(int_values, available_cases - cases_to_take)
+            result += self.get_values_ratio_list(int_values, available_cases - cases_to_take)
 
         return result
--- tests/core/utils/values_ratio_lister_test.py
+++ tests/core/utils/values_ratio_lister_test.py
@@ -6,32 +6,38 @@ from core.utils.ValuesRatioLister import ValuesRatioLister
 class ValuesRatioListerTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.__cardSelector = ValuesRatioLister([-1], 1 / 2)
+        self.__cardSelector = ValuesRatioLister(1 / 2)
 
-    def test_get_list_validations_should_return_list_length_100(self):
+    def test_get_list_validations_should_return_default_list_length_100(self):
 
-        result = self.__cardSelector.get_random_list([-1])
+        result = self.__cardSelector.get_values_ratio_list([-1])
 
         self.assertEqual(len(result), 100)
 
+    def test_get_list_validations_should_return_list_length_define_in_parameter(self):
+
+        result = self.__cardSelector.get_values_ratio_list([-1], 176)
+
+        self.assertEqual(len(result), 176)
+
     def test_get_list_validations_should_return_list_same_number_when_cards_only_minus_one_validation_level(self):
-        result = self.__cardSelector.get_random_list([-1])
+        result = self.__cardSelector.get_values_ratio_list([-1])
 
         self.assertTrue(all(map(lambda number: number == -1, result)))
 
     def test_get_list_validations_should_return_list_same_number_when_cards_only_have_same_validation_level(self):
         self.__cardSelector.values = [0]
-        result = self.__cardSelector.get_random_list([0])
+        result = self.__cardSelector.get_values_ratio_list([0])
         self.assertTrue(all(map(lambda number: number == 0, result)))
 
         self.__cardSelector.values = [1]
-        result = self.__cardSelector.get_random_list([1])
+        result = self.__cardSelector.get_values_ratio_list([1])
         self.assertTrue(all(map(lambda number: number == 1, result)))
 
     def test_get_list_validations_should_return_50_cards_when_ratio_half_and_2_different_validation_level(self):
         self.__cardSelector.values = [-1, 0]
 
-        result = self.__cardSelector.get_random_list([-1, 0])
+        result = self.__cardSelector.get_values_ratio_list([-1, 0])
 
         self.assertEqual(len([number for number in result if number == -1]), 50)
         self.assertEqual(len([number for number in result if number == 0]), 50)
@@ -39,7 +45,7 @@ class ValuesRatioListerTest(unittest.TestCase):
     def test_get_list_validations_should_return_50_cards_when_ratio_half_and_2_not_successive_validation_level(self):
         self.__cardSelector.values = [-1, 1]
 
-        result = self.__cardSelector.get_random_list([-1, 1])
+        result = self.__cardSelector.get_values_ratio_list([-1, 1])
 
         self.assertEqual(len([number for number in result if number == -1]), 50)
         self.assertEqual(len([number for number in result if number == 0]), 0)
@@ -49,7 +55,7 @@ class ValuesRatioListerTest(unittest.TestCase):
         self.__cardSelector.values = [-1, 0, 2]
         self.__cardSelector.ratio = 3 / 4
 
-        result = self.__cardSelector.get_random_list([-1, 0, 2])
+        result = self.__cardSelector.get_values_ratio_list([-1, 0, 2])
 
         self.assertEqual(len([number for number in result if number == -1]), 75)
         self.assertEqual(len([number for number in result if number == 0]), 19)
@@ -60,7 +66,7 @@ class ValuesRatioListerTest(unittest.TestCase):
         self.__cardSelector.values = [-1, 0, 1, 2, 3, 4]
         self.__cardSelector.ratio = 2 / 3
 
-        result = self.__cardSelector.get_random_list([-1, 0, 1, 2, 3, 4])
+        result = self.__cardSelector.get_values_ratio_list([-1, 0, 1, 2, 3, 4])
 
         self.assertEqual(len([number for number in result if number == -1]), 67)
         self.assertEqual(len([number for number in result if number == 0]), 22)
@@ -73,7 +79,7 @@ class ValuesRatioListerTest(unittest.TestCase):
         self.__cardSelector.values = [4]
         self.__cardSelector.ratio = 5 / 6
 
-        result = self.__cardSelector.get_random_list([4])
+        result = self.__cardSelector.get_values_ratio_list([4])
 
         self.assertEqual(len([number for number in result if number == -1]), 0)
         self.assertEqual(len([number for number in result if number == 0]), 0)
