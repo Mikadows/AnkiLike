91c61c59b2c20a688ba13a492f9a0b350f0e3457
MasatakaIshii <masaradio78@gmail.com>
2020-07-09 12:58:28 +0200

    Remove deck index param ioon get_card_index_randomly

--- ui/playerviews/QuestionController.py
+++ ui/playerviews/QuestionController.py
@@ -15,7 +15,7 @@ class QuestionController:
     current_card_index = property(_get_current_card_index, _set_current_card_index)
 
     def draw_card(self):
-        self.current_card_index = self.card_service.get_card_index_randomly(self.deck_index)
+        self.current_card_index = self.card_service.get_card_index_randomly()
         return self.card_service.data.box.decks[self.deck_index].cards[self.current_card_index]
 
     def update_card_validation_level(self, is_validate: bool, master=None):
